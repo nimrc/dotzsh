1d951fda3f5b4c46ea21528048da477d3e7cdcf2
fyibmsd <fyibmsd@gmail.com>
2018-04-07 21:22:40 +0800

    update

--- init.sh
+++ init.sh
@@ -3,8 +3,11 @@
 rm -rf ~/.zsh ~/.oh-my-zsh ~/.zshrc
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# check os release
+os=$(curl -sL https://raw.githubusercontent.com/fyibmsd/server-scripts/master/os.sh | bash -)
 # install plugins
-git clone --depth=1 https://github.com/fyibmsd/dotzsh.git ~/.zsh
+git clone --depth=1 -b $os https://github.com/fyibmsd/dotzsh.git ~/.zsh
+
 cd ~/.zsh
 # local config
 touch .zsh_local
